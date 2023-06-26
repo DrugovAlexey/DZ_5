49215f518627d62131368772dbb86b7ec8030674
Olga <panda146@yandex.ru>
2023-06-26 13:21:53 +0300

    2

---
+++ Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] GenerateArray(int len, int min, int max)		// Создает и наполняет массив
+{
+    int[] list1 = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Length; i++)
+    {
+        list1[i] = rnd.Next(min, max + 1);
+    }
+    return list1;
+}
+
+void PrintArrey(int[] Arrey)				// Показывает в терминале массив
+{
+    for (int i = 0; i < Arrey.Length; i++)
+    {
+        System.Console.Write(Arrey[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int count(int[] arrey)
+{
+    int num = 0;
+
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            num = num + arrey[i];
+        }
+    }
+    return num;
+}
+
+int[] numberlist = GenerateArray(4, -10, 10);
+PrintArrey(numberlist);
+System.Console.WriteLine(count(numberlist));
+
